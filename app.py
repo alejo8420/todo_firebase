e9d060c86f149f9d4d67c67a51298e1e68da1e45
alejo8420 <alejandro.salazar8420@gmail.com>
2022-09-20 20:50:35 -0500

    FC

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from firebase_admin import credentials, firestore, initialize_app
 app = Flask(__name__)
 
 # Initialize Firestore DB
-cred = credentials.Certificate('flask-api-todo-firebase-firebase-adminsdk-whbps-56e8e8ced9.json')
+cred = credentials.Certificate('todo-firebase8420-firebase-adminsdk-auptm-977346351a.json')
 default_app = initialize_app(cred)
 db = firestore.client()
 todo_ref = db.collection('todos')
